d1c192b4e18da9f8b199ce7a5c3f9877bce30bac
Martin Spišák <martin.spisak@glami.cz>
2023-12-30 20:12:24 +0100

    1.1 - comments

---
+++ comments/comments.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // sample line comment
+
+    /*
+    sample comment block
+
+    /* can be nested */
+
+    */
+
+    // block comment can be used for disabling chunks of code
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
