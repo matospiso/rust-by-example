e4c203960946f1ff3ce96cc74ea8bf57b67670fd
Martin Spišák <martin.spisak@glami.cz>
2023-12-30 19:50:29 +0100

    1 - hello world

---
+++ hello-world/hello-world.rs
@@ -0,0 +1,15 @@
+// example comment
+
+// main function
+fn main() {
+    // statements here are executed when the compiled binary is called
+
+    // print text to stdout
+    println!("Hello World"); // don't forget semicolons
+    // `println!` is a macro
+    println!("printing is easy")
+}
+
+// compiling using Rust compiler `rustc`:
+// `rustc hello-world.rs`
+// -> creates an executable `hello-world` binary
